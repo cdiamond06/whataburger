90b506e4db8a7f2c38fefef2edd494f4228c7321
Clinton Diamond <clintondiamond@Clinton-Diamonds-MacBook-Pro.local>
2017-05-22 15:42:36 -0700

    prefinal version with heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
 INSERT INTO burgers (burger_name) VALUES ('Double Double');
 INSERT INTO burgers (burger_name) VALUES ('Mcdouble');
 INSERT INTO burgers (burger_name) VALUES ('Whooper');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
